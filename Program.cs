cc13190a56284d0d60b61201128aa80f333bf92a
최우혁 <stcwh@naver.com>
2023-05-25 05:44:28 +0900

    TEST001

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Test01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine("HeLLo WorLD!");
+            Console.WriteLine("Hello Github");
+        }
+    }
+}
