37a99297df7e098150dca515a8291d3048475dd6
최우혁 <stcwh@naver.com>
2023-05-25 05:51:14 +0900

    NewTEST001

---
+++ GithubTEST001/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //this is brand new test done by me
+        }
+    }
+}
